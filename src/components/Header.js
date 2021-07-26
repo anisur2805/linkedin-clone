f78427509b9ed672b0257d41a09c2608eabaa33f
anisur2805 <anisur2805@gmail.com>
2021-07-27 01:20:33 +0600

    [fix] header topbar style

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ import logo from '../logo.png';
 import './Header.css'
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@material-ui/icons/Home';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import NotificationIcon from '@material-ui/icons/Notifications';
+import ChatIcon from '@material-ui/icons/Chat';
 
 function Header() {
   return (
@@ -16,11 +19,11 @@ function Header() {
         </div>
       </div>
       <div className="header__right">
-        <HeaderOption title="Something" Icon={HomeIcon}></HeaderOption>  
-        <HeaderOption title="Something" Icon={HomeIcon}></HeaderOption>  
-        <HeaderOption title="Something" Icon={HomeIcon}></HeaderOption>  
-        <HeaderOption title="Something" Icon={HomeIcon}></HeaderOption>  
-        <HeaderOption title="Me" Icon={HomeIcon}></HeaderOption>  
+        <HeaderOption title="Home" Icon={HomeIcon}></HeaderOption>  
+        <HeaderOption title="My Network" Icon={SupervisedUserCircleIcon}></HeaderOption>  
+        <HeaderOption title="Messaging" Icon={ChatIcon}></HeaderOption>  
+        <HeaderOption title="Notification" Icon={NotificationIcon}></HeaderOption>  
+        <HeaderOption title="Me" avatar="https://media-exp1.licdn.com/dms/image/C5103AQExs31ZUfBz8g/profile-displayphoto-shrink_100_100/0/1561810463192?e=1632960000&v=beta&t=mP5kRDomPl1G1gsM7AL4LtQ_KjT-3LVcFOUoyYdJpoA"></HeaderOption>  
       </div>
     </div>
   )
