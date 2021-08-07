3bd976b508bd423e2f4c2afffd60e2adbea94124
anisur2805 <anisur2805@gmail.com>
2021-08-07 09:45:15 +0600

    working on Feed JS

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -10,89 +10,83 @@ import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Post from "./Post";
 
 function Feed() {
-  const [ input, setInput ] = useState('');
-  const [ posts, setPosts ] = useState([]);
+	const [posts, setPosts] = useState([]);
+	const [input, setInput] = useState("");
 
-  useEffect(() => {
-    db.collection("posts")
-      .orderBy("timestamp", "desc")
-      .onSnapshot((snapshot) =>
-        setPosts(
-          snapshot.docs.map((doc) => ({
-            id: doc.id,
-            data: doc.data(),
-          }))
-        )
-      );
-  }, []);
+	useEffect(() => {
+		db.collection("posts")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) =>
+				setPosts(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						data: doc.data(),
+					}))
+				)
+			);
+	}, []);
 
-  const sendPost = (e) => {
-    e.preventDefault();
+	const sendPost = (e) => {
+		e.preventDefault();
 
-    db.collection("posts").add({
-      name: "Anisur Rahman",
-      description: "this is a test",
-      message: input,
-      photoUrl: "",
-      timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });
+		db.collection("posts").add({
+			name: "Anisur Rahman",
+			description: "this is a test",
+			message: input,
+			photoUrl: "",
+			timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
 
-    setInput("");
-  };
-  return (
-    <div className="feed">
-      <div className="feed__inputContainer">
-        <div className="feed__input">
-          <CreateIcon />
-          <form>
-            <input
-              value={ input }
-              onChange={ (e) => setInput(e.target.value) }
-            />
-            <button type="submit" onClick={ sendPost } />
-          </form>
-        </div>
+		setInput("");
+		console.log("posts", posts);
+	};
+	return (
+		<div className="feed">
+			<div className="feed__inputContainer">
+				<div className="feed__input">
+					<CreateIcon />
+					<form>
+						<input
+							value={input}
+							onChange={(e) => setInput(e.target.value)}
+						/>
+						<button type="submit" onClick={sendPost} />
+					</form>
+				</div>
+			</div>
 
-        <div className="feed__inputOptions">
-          <InputOption
-            Icon={ ImageIcon }
-            title="Photo"
-            color="#70B5F9"
-          />
-          <InputOption
-            Icon={ SubscriptionsIcon }
-            title="Video"
-            color="#70B5F9"
-          />
-          <InputOption
-            Icon={ EventNoteIcon }
-            title="Event"
-            color="#70B5F9"
-          />
-          <InputOption
-            Icon={ CalendarViewDayIcon }
-            title="Write Article"
-            color="#70B5F9"
-          />
-        </div>
+			<div className="feed__inputOptions">
+				<InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+				<InputOption
+					Icon={SubscriptionsIcon}
+					title="Video"
+					color="#70B5F9"
+				/>
+				<InputOption
+					Icon={EventNoteIcon}
+					title="Event"
+					color="#70B5F9"
+				/>
+				<InputOption
+					Icon={CalendarViewDayIcon}
+					title="Write Article"
+					color="#70B5F9"
+				/>
+			</div>
 
-        { posts.map(
-          ({
-            id,
-            data: { name, description, message, photoUrl },
-          }) => (
-            <Post
-              key={ id }
-              name={ name }
-              description={ description }
-              message={ message }
-              photoUrl={ photoUrl }
-            />
-          )
-        ) }
-      </div>
-    </div>
-  );
+			{posts.map(
+				({ id, data: { name, description, message, photoUrl } }) => (
+					<Post
+						key={id}
+						name={name}
+						description={description}
+						message={message}
+						photoUrl={photoUrl}
+					/>
+				)
+			)}
+		</div>
+	);
 }
 
 export default Feed;
